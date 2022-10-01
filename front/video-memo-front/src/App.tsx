259181cbce899952d8e6968c9667ca8e7a486908
bishil06 <bishil06@naver.com>
2022-10-01 10:53:37 +0900

    feat loadMemoListFromFile saveMemoListToFile

--- front/video-memo-front/src/App.tsx
+++ front/video-memo-front/src/App.tsx
@@ -60,6 +60,33 @@ function App() {
     })
   }
 
+  const loadMemoListFromFile = () => {
+    window.showOpenFilePicker()
+      .then(([fileHandle]) => fileHandle.getFile())
+      .then(file => new Response(file))
+      .then(res => res.json())
+      .then(arr => {
+        if (arr instanceof Array) {
+          setMemoList(arr)
+        }
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
+
+  const saveMemoListToFile = () => {
+    window.showSaveFilePicker({
+      suggestedName: 'memoList.json'
+    })
+      .then((fileHandle) => fileHandle.createWritable())
+      .then(wstream => wstream.getWriter())
+      .then(writer => writer.write(JSON.stringify(memoList)).then(() => writer.close()))
+      .catch(err => {
+        console.error(err)
+      })
+  }
+
   const deleteMemo = (obj: { [k: string]: string | number }) => {
     const newList = memoList.filter(memo => obj !== memo)
     setMemoList(newList)
@@ -82,7 +109,7 @@ function App() {
       <h1>Video Memo</h1>
       <div className='inputContainer'>
         <VideoPathInputContainer setVideoPath={setVideoPath} videoPath={videoPath}></VideoPathInputContainer>
-        <LoadMemoListContainer loadMemoList={loadMemoList}></LoadMemoListContainer>
+        <LoadMemoListContainer loadMemoList={loadMemoList} loadMemoListFromFile={loadMemoListFromFile} saveMemoListToFile={saveMemoListToFile}></LoadMemoListContainer>
       </div>
       <VideoContainer memoList={memoList} pushMemoList={pushMemoList} deleteMemo={deleteMemo} appSize={appSize} videoPath={videoPath}></VideoContainer>
     </div>
--- front/video-memo-front/src/LoadMemoListContainer.tsx
+++ front/video-memo-front/src/LoadMemoListContainer.tsx
@@ -1,7 +1,9 @@
-export default function LoadMemoListContainer({ loadMemoList }: { loadMemoList: ()=>void}) {
+export default function LoadMemoListContainer({ loadMemoList, loadMemoListFromFile, saveMemoListToFile }: { loadMemoList: ()=>void, loadMemoListFromFile: ()=>void, saveMemoListToFile: ()=>void }) {
   return (
     <div className="loadMemoListContainer">
       <button onClick={() => loadMemoList()}>현재 url 메모 불러오기</button>
+      <button onClick={() => loadMemoListFromFile()}>메모파일 불러오기</button>
+      <button onClick={() => saveMemoListToFile()}>현재 메모리스트 파일로 저장</button>
     </div>
   )
 }
