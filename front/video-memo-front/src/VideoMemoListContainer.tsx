55d2b18d6abb4ac657ac29348c6321b85333e4ea
bishil06 <bishil06@naver.com>
2022-10-03 09:34:03 +0900

    fix memoList Size

--- front/video-memo-front/src/VideoMemoListContainer.tsx
+++ front/video-memo-front/src/VideoMemoListContainer.tsx
@@ -1,21 +1,27 @@
 import MemoCard from "./MemoCard"
 import { VariableSizeList } from 'react-window'
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 
 export default function VideoMemoListContainer({ memoList, width, seekTo, deleteMemo }: { memoList: Array<{ [k: string]: string | number }>, width: number, seekTo: (s: number)=>void, deleteMemo: (obj: { [k: string]: string | number })=>void }) {
   const containerRef = useRef<HTMLDivElement>(null) 
+  const variableSizeList = useRef<VariableSizeList>(null)
 
   const renderRow = ({ index, style }: { index: number, style: {} }) => {
     return <MemoCard style ={style} memo={memoList[index]} key={index} seekTo={seekTo} deleteMemo={deleteMemo}></MemoCard>
   }
 
   function calcSize(index: number) {
+    console.log(memoList[index], (Object.keys(memoList[index]).length) * 30)
     return (Object.keys(memoList[index]).length) * 30
   }
 
+  useEffect(() => {
+    variableSizeList.current?.resetAfterIndex(0)
+  }, [memoList])
+
   return (
   <div ref={containerRef} className="videoMemoListContainer" style={{ width, height: '100vh'}}>
-    <VariableSizeList height={containerRef.current?.offsetHeight || 1000} width={width} itemCount={memoList.length} itemSize={calcSize}>
+    <VariableSizeList ref={variableSizeList} height={containerRef.current?.offsetHeight || 1000} width={width} itemCount={memoList.length} itemSize={calcSize}>
       {renderRow}
     </VariableSizeList>
   </div>
